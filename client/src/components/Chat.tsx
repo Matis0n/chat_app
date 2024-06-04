f153d534f7a495fba43bb4269f8c9b46bb00c4cf
Evgeny Zherdev <zheny-ye@bk.ru>
2024-06-04 15:19:51 +0700

    b1 #5 update and add logic

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,19 +1,23 @@
 import {useEffect, useState} from "react";
 import {io} from 'socket.io-client';
 import {useLocation} from "react-router-dom";
-import {FieldsType} from "../types/types.ts";
+import {UserTypeFromServer, FieldsType} from "../types/types.ts";
 
+import styles from '../styles/Chat.module.css'
+import emoji from '../assets/img/emoji.svg'
+import EmojiPicker from "emoji-picker-react";
 
 let socket = io('http://localhost:5000');
 
 type searchParamsType = Record<string, string>
 
-
 function Chat() {
-    const [state, setState] = useState<FieldsType[]>([])
-    let {search} = useLocation()
-    const [params, setParams] = useState<searchParamsType | null>(null);
+    const [state, setState] = useState<UserTypeFromServer[]>([])
+    const [params, setParams] = useState<FieldsType | searchParamsType>({room: '', user: ''});
+    const [message, setMessage] = useState<string>('')
+    const [isOpen, setOpen] = useState<boolean>(false)
 
+    let {search} = useLocation()
     useEffect(() => {
         const searchParams = Object.fromEntries(new URLSearchParams(search))
         setParams(searchParams);
@@ -22,16 +26,51 @@ function Chat() {
 
     useEffect(() => {
         socket.on('message', ({data}) => {
-            setState((_state) => ({..._state, data}))
+            setState((_state) => ([..._state, data]))
         })
     }, []);
+    console.log('state', state)
+    console.log('params', params)
 
-    console.log(state)
-    console.log(params)
+    const leftRoom = () => {}
+    const handleChange = () => {}
+    const onEmojiClick = () => setOpen(!isOpen)
+    const handleSubmit = ()=>{}
     return (
-        <>
-            Chat
-        </>
+        <div className={styles.wrap}>
+            <div className={styles.header}>
+                <div className={styles.title}>Комната: {params.room}</div>
+                <div className={styles.users}>{} 0 человек в комнате</div>
+                <button className={styles.left} onClick={leftRoom}>Выйти из комнаты</button>
+            </div>
+            <div className={styles.messages}>
+                {state.map(({user}) => <div>{user.message}</div>)}
+            </div>
+            <form className={styles.form}>
+                <div className={styles.input}>
+                    <input
+                        type="text"
+                        placeholder="Напишите сообщение..."
+                        name="message"
+                        value={message}
+                        onChange={handleChange}
+                        autoComplete="off"
+                        required
+                    />
+                </div>
+                <div className={styles.emoji}>
+                    <img src={emoji} alt="emoji" onClick={onEmojiClick}/>
+                    {isOpen && (
+                        <div className={styles.emojies}>
+                            <EmojiPicker onEmojiClick={onEmojiClick}/>
+                        </div>
+                    )}
+                </div>
+                <div className={styles.button}>
+                    <input type="submit" onSubmit={handleSubmit} value="Отправить"/>
+                </div>
+            </form>
+        </div>
     )
 }
 
